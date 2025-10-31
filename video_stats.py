d3ffbdbc010890dcd84764c2aa158dbdd9b34890
yanborges <yangborges@gmail.com>
2025-10-31 12:11:51 -0300

    Added get_video_ids function to video_stats file

--- video_stats.py
+++ video_stats.py
@@ -7,8 +7,9 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
-
 CHANNEL_HANDLE = "mkbhd"
+maxResults = 50
+
 
 def get_playlist_id():
 
@@ -16,6 +17,8 @@ def get_playlist_id():
         url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
 
         response = requests.get(url)
+
+        response.raise_for_status()
 
         # print(response)
 
@@ -29,7 +32,7 @@ def get_playlist_id():
 
         channel_playlistID = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
 
-        print(channel_playlistID)
+        # print(channel_playlistID)
 
         return channel_playlistID
     
@@ -37,8 +40,53 @@ def get_playlist_id():
         print(f"An error occurred while fetching the playlist ID: {e}")
         return None
     
+
+
+    
+def get_video_ids(playlistId):
+
+    video_ids = [] 
+
+    pageToken = None 
+
+    base_url =   f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+
+    try: 
+
+        while True: 
+
+            url = base_url 
+
+            # Get the first page of results, then subsequent pages using the pageToken
+            if pageToken: 
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id) 
+
+            pageToken = data.get("nextPageToken")
+
+            if not pageToken: 
+                break 
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching video IDs: {e}")
+        return None
+
 if __name__ == "__main__":
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    get_video_ids(playlistId)
+    
+
 
 
 
